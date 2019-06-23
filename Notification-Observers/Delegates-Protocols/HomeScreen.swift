48735161d2f150054524eb71087c278e4919bdb2
Tim Miller <millman@icloud.com>
2019-06-23 01:07:10 -0400

    Fix auto layout

---
+++ Notification-Observers/Delegates-Protocols/HomeScreen.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Delegates-Protocols
+//
+//  Created by Tim Miller on 5/14/19.
+//  Copyright © 2019 Tim Miller. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var chooseButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        chooseButton.layer.cornerRadius = chooseButton.frame.size.height / 2
+    }
+    
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
+        present(selectionVC, animated: true, completion: nil)
+    }
+}
--- Notification-Observers/Delegates-Protocols/SelectionScreen.swift
+++ Notification-Observers/Delegates-Protocols/SelectionScreen.swift
@@ -8,13 +8,7 @@
 
 import UIKit
 
-protocol SideSelectionDelegate {
-    func didTapChoice(image: UIImage, name: String, color: UIColor)
-}
-
 class SelectionScreen: UIViewController {
-
-    var selectionDelegate: SideSelectionDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +17,10 @@ class SelectionScreen: UIViewController {
     }
     
     @IBAction func imperialButtonTapped(_ sender: UIButton) {
-        selectionDelegate.didTapChoice(image: UIImage(named: "vader")!, name: "Darth Vader", color: .red)
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func rebelButtonTapped(_ sender: UIButton) {
-        selectionDelegate.didTapChoice(image: UIImage(named: "luke")!, name: "Luke Skywalker", color: .cyan)
         dismiss(animated: true, completion: nil)
-    }
-    
-    deinit {
-        print("Reclaiming memory")
     }
 }
